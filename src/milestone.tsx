e475d30b3b546f24da5424fa8d489f36494f9070
Ryan Nguyen <th.nguyen.developer@gmail.com>
2025-03-30 16:58:21 -0700

    Add recharts dependency, update chart colors to pure red and green, and add contribution data files

--- src/milestone.tsx
+++ src/milestone.tsx
@@ -6,9 +6,11 @@ import {
   CardHeader,
   CardTitle
 } from "@/components/ui/card"
-import { Carousel, CarouselApi, CarouselContent, CarouselItem } from './components/ui/carousel'
+import { CarouselApi, CarouselItem } from './components/ui/carousel'
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts"
 import { ScrollArea } from './components/ui/scroll-area'
 import { useCallback, useEffect, useState } from 'react';
+import { ChartContainer, ChartTooltip, ChartTooltipContent } from './components/ui/chart'
 
 export interface MilestoneProps {
   owner: string;
@@ -19,9 +21,6 @@ export interface MilestoneProps {
   subtittle: string;
 
   description: string;
-
-  imageDescription: [string];
-  imageLink: [string];
 }
 
 interface GitHubCommitAuthor {
@@ -81,15 +80,16 @@ interface GitHubCommit {
 function Milestone({content}: {content: MilestoneProps}) {
   // Fixed the CarouselApi state declaration with proper type
   const [carouselApi, setCarouselApi] = useState<CarouselApi | null>(null);
-  const [carouselCurrentIndex, setCarouselCurrentIndex] = useState(0);
-  const [carouselItems, setCarouselItems] = useState(0);
+  // const [carouselCurrentIndex, setCarouselCurrentIndex] = useState(0);
+  // const [carouselItems, setCarouselItems] = useState(0);
   const [commits, setCommits] = useState<GitHubCommit[]>([]);
+  const [chartData, setChartData] = useState([]);
 
   useEffect(() => {
     async function fetchCommits() {
 
     try {
-      const cachedDataUrl = `${import.meta.env.BASE_URL}data/${content.owner}-${content.repo}-${content.branch}.json`;
+      const cachedDataUrl = `${import.meta.env.BASE_URL}data/${content.owner}-${content.repo}-${content.branch}-commits.json`;
       const response = await fetch(cachedDataUrl);
       if (response.ok) {
         const data = await response.json();
@@ -104,42 +104,84 @@ function Milestone({content}: {content: MilestoneProps}) {
   fetchCommits();
   },[content]);
 
-  // Set up an effect to create a timer that scrolls to the next carousel item
   useEffect(() => {
-    if (!carouselApi) return;
-    
-    // Create an interval that calls scrollNext every 3 seconds
-    const interval = setInterval(() => {
-      carouselApi.scrollNext();
-      const currentIndex = carouselApi.selectedScrollSnap();
-      setCarouselCurrentIndex(currentIndex);
-    }, 4000);
-    
-    // Clean up the interval when component unmounts
-    return () => clearInterval(interval);
-  }, [carouselApi]);
+    async function fetchContribution() {
 
-  useEffect(() => {
-    if (!carouselApi) return;
-    setCarouselCurrentIndex(carouselApi.selectedScrollSnap());
-    setCarouselItems(carouselApi.slideNodes().length);
-  }, [carouselApi]);
+    try {
+      const cachedDataUrl = `${import.meta.env.BASE_URL}data/${content.owner}-${content.repo}-${content.branch}-contribution.json`;
+      const response = await fetch(cachedDataUrl);
+      if (response.ok) {
+        const data = await response.json();
+        const transformedData = data[0].weeks.map((week: any) => ({
+          w: new Date(week.w * 1000).toLocaleDateString(),
+          additions: week.a,
+          deletions: week.d,
+        }));
+        console.log(transformedData);
+        setChartData(transformedData);
+      } else {
+        console.error(`Failed to fetch commits: ${response.status} ${response.statusText}`);
+      }
+    } catch (error) {
+      console.error("Error fetching commits:", error);
+    }
+  }
+  fetchContribution();
+  },[content]);
+
+  // Set up an effect to create a timer that scrolls to the next carousel item
+  // useEffect(() => {
+  //   if (!carouselApi) return;
+    
+  //   // Create an interval that calls scrollNext every 3 seconds
+  //   const interval = setInterval(() => {
+  //     carouselApi.scrollNext();
+  //     const currentIndex = carouselApi.selectedScrollSnap();
+  //     setCarouselCurrentIndex(currentIndex);
+  //   }, 4000);
+    
+  //   // Clean up the interval when component unmounts
+  //   return () => clearInterval(interval);
+  // }, [carouselApi]);
+
+  // useEffect(() => {
+  //   if (!carouselApi) return;
+  //   setCarouselCurrentIndex(carouselApi.selectedScrollSnap());
+  //   setCarouselItems(carouselApi.slideNodes().length);
+  // }, [carouselApi]);
 
   const commitsDisplay = useCallback(() => {
     if (!commits) return null;
+    const limitWords = (text: string, wordLimit: number = 15): string => {
+      const words = text.split(/\s+/);
+      if (words.length <= wordLimit) return text;
+      return words.slice(0, wordLimit).join(' ') + '...';
+    };
     const commitList = commits.map((commit, index) => (
-      <div key={index} className='w-full flex flex-col'>
+      <div key={index} className='w-full flex flex-col space-y-1'>
         <div className='w-full h-0.5 bg-gray-300 rounded-full pr-3'></div>
-        <a 
-          className='w-full text-sm/tight font-semibold text-green-600 hover:text-green-500 transition-colors line-clamp-2' 
-          href={commit.html_url}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          {commit.commit.message}
-        </a>
+        <div className='flex flex-row space-x-1 w-full'>
+          <img
+            src={commit.author?.avatar_url || 'https://via.placeholder.com/150'}
+            alt={commit.commit.author.name}
+            className='w-8 h-8 rounded-full mb-2 border-1 text-gray-300'
+          >
+          </img>
+          <a  
+            href={commit.html_url}
+            target='_blank'
+            rel='noopener noreferrer'
+            className='w-[calc(100%-2rem)]'
+          >
+            <p className='w-full line-clamp-2 text-green-600 hover:text-green-500   transition-colors text-sm/tight font-semibold'>
+              {limitWords(commit.commit.message, 12)} 
+            </p>
+          </a>
+        </div>
+        <div className='flex-row flex justify-between'>
         <p className='text-xs text-gray-300'>{commit.commit.author.name}</p>
         <p className='text-xs text-gray-300'>{new Date(commit.commit.author.date).toLocaleDateString()}</p>
+        </div>
       </div>
     ));
     return (
@@ -149,34 +191,47 @@ function Milestone({content}: {content: MilestoneProps}) {
     );
   }, [commits]);
 
-  const carouselImageDisplay = useCallback(() => {
-    if (!carouselApi) return null;
-    const { imageLink, imageDescription } = content;
-    const images = imageLink.map((imageUrl, index) => (
-      <CarouselItem key={index} className='w-full h-full'>
-        <img
-          src={imageUrl}
-          alt={imageDescription[index] || ''}
-          className="object-cover w-full h-full"
-        />
-      </CarouselItem>
-    ));
-    return images;
-  }, [carouselApi, content]);
+  
+  const chartConfig = {
+    additions: {
+      label: "Additions",
+      color: "var(--chart-1)",
+    },
+    deletions: {
+      label: "Deletions",
+      color: "var(--chart-2)",
+    },
+  } 
 
 
-  const carouselAsDots = useCallback(() => {
-    if (!carouselApi) return null;
-    const totalItems = carouselItems;
-    const currentIndex = carouselCurrentIndex;
-    const dots = Array.from({ length: totalItems }, (_, index) => (
-      <div
-        key={index}
-        className={`h-2 w-2 rounded-full ${index === currentIndex ? 'bg-blue-500' : 'bg-gray-300'}`}
-      ></div>
-    ));
-    return <div className="flex-col flex space-y-2 h-full justify-center items-center">{dots}</div>;
-  }, [carouselApi, carouselCurrentIndex, carouselItems]);
+  // const carouselImageDisplay = useCallback(() => {
+  //   if (!carouselApi) return null;
+  //   const { imageLink, imageDescription } = content;
+  //   const images = imageLink.map((imageUrl, index) => (
+  //     <CarouselItem key={index} className='w-full h-full'>
+  //       <img
+  //         src={imageUrl}
+  //         alt={imageDescription[index] || ''}
+  //         className="object-cover w-full h-full"
+  //       />
+  //     </CarouselItem>
+  //   ));
+  //   return images;
+  // }, [carouselApi, content]);
+
+
+  // const carouselAsDots = useCallback(() => {
+  //   if (!carouselApi) return null;
+  //   const totalItems = carouselItems;
+  //   const currentIndex = carouselCurrentIndex;
+  //   const dots = Array.from({ length: totalItems }, (_, index) => (
+  //     <div
+  //       key={index}
+  //       className={`h-2 w-2 rounded-full ${index === currentIndex ? 'bg-blue-500' : 'bg-gray-300'}`}
+  //     ></div>
+  //   ));
+  //   return <div className="flex-col flex space-y-2 h-full justify-center items-center">{dots}</div>;
+  // }, [carouselApi, carouselCurrentIndex, carouselItems]);
 
   return (
     <div>
@@ -192,22 +247,30 @@ function Milestone({content}: {content: MilestoneProps}) {
             <div className='w-1/4 h-h-full text-left'>
               <p>{content.description}</p>
             </div>
-              <div className='w-2/4 flex-row flex justify-center items-center h-full space-x-1 overflow-y-hidden'>
-                <Carousel 
-                setApi={setCarouselApi} opts={
-                  {
-                    loop: true,
-                  }
-                } 
-                className='w-full justify-center items-center'>
-                  <CarouselContent className='w-full h-full'>
-                    {carouselImageDisplay()}
-                  </CarouselContent>
-                </Carousel>
-                {carouselAsDots()}
-              </div>
-            <ScrollArea className='h-full w-1/4'> 
-                <div className='w-full'>
+            <ChartContainer config={chartConfig} className='w-2/4 h-full'>
+              <BarChart accessibilityLayer data={chartData}>
+                <CartesianGrid vertical={false} />
+                <XAxis
+                  dataKey="w"
+                  tickLine={false}
+                  tickMargin={10}
+                  axisLine={false}
+                />
+                <YAxis
+                  tickLine={false}
+                  tickMargin={10}
+                  axisLine={false}
+                ></YAxis>
+                <ChartTooltip
+                  cursor={false}
+                  content={<ChartTooltipContent indicator="dashed" />}
+                />
+                  <Bar dataKey="additions" fill="var(--chart-1)" radius={4} />
+                  <Bar dataKey="deletions" fill="var(--chart-2)" radius={4} />
+              </BarChart>
+            </ChartContainer>
+            <ScrollArea className='h-full w-1/4 px-2'> 
+                <div className='w-full pr-2'>
                   {commitsDisplay()}
                 </div>
             </ScrollArea>
